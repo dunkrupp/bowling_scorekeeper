efa10c02480425152296cb5ce8dc13e71396fd43
Drew Ruppel <drewruppel@gmail.com>
2025-02-19 13:31:08 -0800

    feat(operations): introduce our roll recorders

---
+++ app/operations/base_operation.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class BaseOperation
+  include Dry::Monads[:do, :result]
+
+  private
+
+  DEFAULT_NIL = -> {}
+
+  # Logs the message to +Rails.logger+ and returns +Failure(message)+
+  #
+  # @param [String|Symbol] message
+  # @param [Exception] exception
+  def capture_and_fail(message, exception)
+    Rails.logger.error(exception.message)
+    Failure(message)
+  end
+end
